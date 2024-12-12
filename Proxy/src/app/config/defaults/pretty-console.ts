6773b15cab9c6d90b6fbda78a6a962ac1deba5a2
Daniele Pizzi <pizzi.daniele1993@gmail.com>
2024-12-12 14:28:13 +0100

    Update pretty-console.ts

--- Proxy/src/app/config/defaults/pretty-console.ts
+++ Proxy/src/app/config/defaults/pretty-console.ts
@@ -1,1 +1,36 @@
+import { BACKGROUND_ASCII_COLORS } from "../../enums/background-ascii-colors.enum";
+import { FOREGROUND_ASCII_COLORS } from "../../enums/foreground-ascii-colors.enum";
+import { TPrettyConsole } from "../../types/pretty-console.type";
 
+export const PRETTY_CONSOLE_DEFAULT: TPrettyConsole = {
+    closeByNewLine: true,
+    level: {
+        info: {
+            label: {
+                message: "INFO",
+                foreground: FOREGROUND_ASCII_COLORS.WHITE,
+                background: BACKGROUND_ASCII_COLORS.CYAN,
+            },
+            foreground: FOREGROUND_ASCII_COLORS.CYAN,
+            background: BACKGROUND_ASCII_COLORS.BLUE,
+        },
+        warning: {
+            label: {
+                message: "WARNING",
+                foreground: FOREGROUND_ASCII_COLORS.WHITE,
+                background: BACKGROUND_ASCII_COLORS.YELLOW,
+            },
+            foreground: FOREGROUND_ASCII_COLORS.YELLOW,
+            background: BACKGROUND_ASCII_COLORS.BLUE,
+        },
+        error: {
+            label: {
+                message: "ERROR",
+                foreground: FOREGROUND_ASCII_COLORS.WHITE,
+                background: BACKGROUND_ASCII_COLORS.RED,
+            },
+            foreground: FOREGROUND_ASCII_COLORS.RED,
+            background: BACKGROUND_ASCII_COLORS.BLUE,
+        },
+    },
+};
