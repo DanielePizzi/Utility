41d416d1a422b9a6327689ec99d1dc5fc39928cd
Daniele Pizzi <pizzi.daniele1993@gmail.com>
2024-12-12 14:14:55 +0100

    Update proxy.type.ts

--- Proxy/src/app/types/proxy.type.ts
+++ Proxy/src/app/types/proxy.type.ts
@@ -1,1 +1,28 @@
+import { TProxyLog } from "./proxy-log.type";
+import { TProxyTarget } from "./proxy-target.type";
 
+export type TProxy = {
+  /**
+   * The port of the proxy.
+   *
+   *
+   * @type {number}
+   */
+  port: number;
+  /**
+   * Target to forward requests to.
+   *
+   * @type {TProxyTarget}
+   */
+  target: TProxyTarget;
+  /**
+   * Extra headers that will be added to the ones already present.
+   * Example: Frontend makes a call to the proxy and has headers `A B C`,
+   * the proxy will add `D E F`,
+   *  the backend will get `A B C D E F`
+   *
+   * @type {({ [key: string]: string | number })}
+   */
+  extraTargetHeaders?: { [key: string]: string | number };
+  log: TProxyLog;
+};
