d21e8b9ff10e94209dda66287d804fbb1448401d
Daniele Pizzi <pizzi.daniele1993@gmail.com>
2024-12-12 14:10:12 +0100

    Update pretty-console-level-options.type.ts

--- Proxy/src/app/types/pretty-console-level-options.type.ts
+++ Proxy/src/app/types/pretty-console-level-options.type.ts
@@ -1,1 +1,10 @@
+import { BACKGROUND_ASCII_COLORS } from "../enums/background-ascii-colors.enum";
+import { FOREGROUND_ASCII_COLORS } from "../enums/foreground-ascii-colors.enum";
+import { TPrettyConsoleLabel } from "./pretty-console-label.type";
 
+
+export type TPrettyConsoleLevelOptions = {
+  foreground?: FOREGROUND_ASCII_COLORS;
+  background?: BACKGROUND_ASCII_COLORS;
+  label?: TPrettyConsoleLabel;
+};
