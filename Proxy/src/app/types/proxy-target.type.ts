cf41b724f12573a346c97dc9efc1f233cccc07f8
Daniele Pizzi <pizzi.daniele1993@gmail.com>
2024-12-12 14:14:01 +0100

    Update proxy-target.type.ts

--- Proxy/src/app/types/proxy-target.type.ts
+++ Proxy/src/app/types/proxy-target.type.ts
@@ -1,1 +1,15 @@
-
+export type TProxyTarget = {
+  /**
+   * Target url.
+   * Example `http://localhost`
+   *
+   * @type {string}
+   */
+  url: string;
+  /**
+   * Target port.
+   *
+   * @type {(number | null)}
+   */
+  port: number | null;
+};
