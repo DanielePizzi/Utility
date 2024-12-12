a015aca5afe688e889d8669d3b8354f6e930ae06
Daniele Pizzi <pizzi.daniele1993@gmail.com>
2024-12-12 14:27:55 +0100

    Update config.ts

--- Proxy/src/app/config/config.ts
+++ Proxy/src/app/config/config.ts
@@ -1,1 +1,21 @@
+import { TConfig } from "../types/config.type";
+import { PRETTY_CONSOLE_DEFAULT } from "./defaults/pretty-console";
 
+export const APP_CONFIG: TConfig = {
+    proxy: {
+        port: 3000,
+        target: {
+            url: "localhost",
+            port: 8080,
+        },
+        extraTargetHeaders: {
+            "BNL-SECURITY-CONTEXT": "changeMe",
+        },
+        log: {
+            headers: false,
+            sourceBody: false,
+            targetBody: true,
+        },
+    },
+    console: PRETTY_CONSOLE_DEFAULT,
+};
