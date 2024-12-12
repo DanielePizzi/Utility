0d54e04c3af55f91d1489557cbf6702945dc98d6
Daniele Pizzi <pizzi.daniele1993@gmail.com>
2024-12-12 14:12:06 +0100

    Update proxy-events.type.ts

--- Proxy/src/app/types/proxy-events.type.ts
+++ Proxy/src/app/types/proxy-events.type.ts
@@ -1,1 +1,14 @@
+import { TProxyRequestEvent } from "./proxy-request-event.type";
+ 
+export type TProxyEvents = {
+  started: () => void;
+  "new-request": (request: TProxyRequestEvent) => void;
+  "end-request": (status: number) => void;
+  "target-called": () => void;
+  "target-body": (body: string) => void;
+  "target-error": () => void;
+  "target-end": (status: number) => void;
+  "source-body": (body: string) => void;
+};
 
+export type TProxyEventsEmptyProps = Record<keyof TProxyEvents, undefined>;
