a1a7cc0addf82e645fc6396ef824a87709cc7af3
Daniele Pizzi <pizzi.daniele1993@gmail.com>
2024-12-12 14:09:46 +0100

    Update pretty-console-label.type.ts

--- Proxy/src/app/types/pretty-console-label.type.ts
+++ Proxy/src/app/types/pretty-console-label.type.ts
@@ -1,1 +1,8 @@
+import { BACKGROUND_ASCII_COLORS } from "../enums/background-ascii-colors.enum";
+import { FOREGROUND_ASCII_COLORS } from "../enums/foreground-ascii-colors.enum";
 
+export type TPrettyConsoleLabel = {
+  message: string;
+  foreground: FOREGROUND_ASCII_COLORS;
+  background: BACKGROUND_ASCII_COLORS;
+};
