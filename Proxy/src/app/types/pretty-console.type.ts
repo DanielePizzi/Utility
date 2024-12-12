12808c5a2c307ced74b78787a6b25290537d5880
Daniele Pizzi <pizzi.daniele1993@gmail.com>
2024-12-12 14:11:24 +0100

    Update pretty-console.type.ts

--- Proxy/src/app/types/pretty-console.type.ts
+++ Proxy/src/app/types/pretty-console.type.ts
@@ -1,1 +1,6 @@
+import { PRETTY_CONSOLE_TYPES } from "../enums/pretty-console-types.enum";
+import { TPrettyConsoleLevelOptions } from "./pretty-console-level-options.type";
 
+export type TPrettyConsoleLevel = {
+  [k in PRETTY_CONSOLE_TYPES]: TPrettyConsoleLevelOptions;
+};
